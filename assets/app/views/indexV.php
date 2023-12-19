95347d3e841a2ed306fc0007cd5f2e54a2f24afc
Yorber <yorberrojas15@gmail.com>
2023-12-19 11:44:15 -0400

    Add index

---
+++ assets/app/views/indexV.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <div class="cont">
+        <div class="subcont">
+
+            <header>
+                <div class="sombra">
+                    <nav class="menu">
+                        <ul>
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="">Inventario</a></li>
+                            <li><a href="">Añadir producto</a></li>
+                        </ul>
+                    </nav>
+
+                    <h1 class="title">la mejor tienda</h1>
+                </div>
+            </header>
+        </div>
+    </div>
+</body>
+</html>
