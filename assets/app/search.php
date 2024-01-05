4d60d2cde40a4b14d034f13a5e6ef3acd99b4eed
Yorber <yorberrojas15@gmail.com>
2024-01-05 16:26:44 -0400

    add ccs of search

--- assets/app/search.php
+++ assets/app/search.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/search.css">
     <title>Busqueda</title>
 </head>
 <body>
@@ -16,7 +17,7 @@
             </nav>
             <div class="header">
                 <header>
-                    <div class="sombra">
+                    <div class="shadow">
                         <div class="title">
                             <h1>Busca el producto que quieres editar</h1>
                         </div>
